b02310ef66b976c78e0cad7a318fb40393d4325d
Marek Kotarba <xname@epic-nigga-mf.local>
2024-09-26 23:02:05 +0200

    some change

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => {
-  
+
   return (
     <div className='home__container'>
       <h1 className='home__title'>LexAdiutor</h1>
-      <p className='home__description'>trwają pracę nad stroną</p>
+      <p className='home__description'>Trwają prace nad stroną</p>
     </div>
   )
 }
