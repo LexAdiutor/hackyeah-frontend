8134aff1e7be6c177ca48957700c9930dd8a29de
Marek Kotarba <xname@epic-nigga-mf.local>
2024-09-26 12:56:11 +0200

    initial design

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='home__container'>
+      <h1 className='home__title'>LexAdiutor</h1>
+      <p className='home__description'>trwają pracę nad stroną</p>
+    </div>
+  )
+}
+
+export default Home
