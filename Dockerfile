9b8ac6649ebb71ce7e5ba724389949b93d38a5ca
Marek Kotarba <xname@epic-nigga-mf.local>
2024-09-27 12:44:16 +0200

    connection setup

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:18
 
 WORKDIR /app
 
+ENV VITE_API_BACKEND_URL https://lexadiutor.pl:3000
+
 COPY package*.json ./
 
 RUN npm install
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => {
+  const handleClick = async () => {
+    try {
+      const data = await fetch(import.meta.env.VITE_API_BACKEND_URL + '/test', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      alert(await data.text());
+    } catch {
+      alert('Błąd po stronie serwera');
+    }
+  }
 
   return (
     <div className='home__container'>
       <h1 className='home__title'>LexAdiutor</h1>
       <p className='home__description'>Trwają prace nad stroną</p>
+      <button onClick={handleClick}>Zaciągnij dane z serwera</button>
     </div>
   )
 }
